70f071d6db9f00219b7a230a1312706f12787f7d
Galdea <gabriel.aldea@gmail.com>
2023-01-23 16:20:20 +0000

    instalado flask

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -3,17 +3,12 @@ import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui
 import 'semantic-ui-css/semantic.min.css'
 
 
-// const [form, setForm] = useState ({
-//     cont onChange = (E,{name, value}) => {
-//         setForm({...form, [name]:value})
-//     }
-// })
 
 export const Register = () => (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
       <Header as='h2' color='teal' textAlign='center'>
-        <Image src='/logo.png' /> Log-in to your account
+        <Image src='/logo.png' /> Register to your account
       </Header>
       <Form size='large'>
     <Segment>
