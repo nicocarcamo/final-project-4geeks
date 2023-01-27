a905d816c565adf1b15beb383e0c180043cfe2ee
Galdea <gabriel.aldea@gmail.com>
2023-01-27 16:43:22 +0000

    eventos y descripcion estan siendo renderizados en unirseevento.js falta aplicar boton para unirse a eventos

--- src/front/js/pages/unirseevento.js
+++ src/front/js/pages/unirseevento.js
@@ -1,62 +1,79 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Table, Button } from 'semantic-ui-react';
-import { Context } from '../store/appContext';
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Form } from 'semantic-ui-react';
 
-export const UnirseEvento = () => {
-  const [eventos, setEventos] = useState([]);
-  const getEvento = async () => {
-    try {
-        const resp = await fetch(
-        process.env.BACKEND_URL + "/api/crearevento",
-        {
-            method: "GET",
-            headers: {
-            "Content-Type": "application/json",
-            },
-        }
-        );
+export function UnirseEvento() {
+    const [events, setEvents] = useState([]);
+    const [nombreevento, setNombreEvento] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [integrantes, setIntegrantes] = useState('');
+    const [publicooprivado, setPublicoOPrivado] = useState('');
+    const [valor, setValor] = useState('');
+    const [ubicacion, setUbicacion] = useState('');
+    const [is_active, setIsActive] = useState('');
 
-        const data = await resp.json();
-        setStore((store) => {
-        store.eventos.push(data);
-
-        return store;
-        });
-    } catch (error) {
-        console.log("Error, no se puede crear evento", error);
-    }
-}
-
-useEffect(() => {
-    getEvento();
-}, [])
-
+    useEffect(() => {
+        fetch(
+            process.env.BACKEND_URL + "/api/crearevento", {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then(res => res.json())
+            .then(data => setEvents(data))
+            .catch(err => console.log(err));
+    }, []);
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        fetch(
+            process.env.BACKEND_URL + "/api/crearevento", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                nombreevento: nombreevento,
+                descripcion: descripcion,
+                integrantes: integrantes,
+                publicooprivado: publicooprivado,
+                valor: valor,
+                ubicacion: ubicacion,
+                is_active: is_active
+            }),
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+    };
+    
     return (
-        <Table>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>Nombre</Table.HeaderCell>
-                    <Table.HeaderCell>Descripcion</Table.HeaderCell>
-                    <Table.HeaderCell>Integrantes</Table.HeaderCell>
-                    <Table.HeaderCell>Valor</Table.HeaderCell>
-                    <Table.HeaderCell>Ubicacion</Table.HeaderCell>
-                    <Table.HeaderCell>Action</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {eventos.map((crearevento) => (
-                    <Table.Row key={crearevento.id}>
-                        <Table.Cell>{crearevento.nombreevento}</Table.Cell>
-                        <Table.Cell>{crearevento.descripcion}</Table.Cell>
-                        <Table.Cell>{crearevento.integrantes}</Table.Cell>
-                        <Table.Cell>{crearevento.valor}</Table.Cell>
-                        <Table.Cell>{crearevento.ubicacion}</Table.Cell>
-                        {/* <Table.Cell>
-                            <Button onClick={() => actions.joinEvent(crearevento.id)}>Join</Button>
-                        </Table.Cell> */}
+        <div>
+            <h1>Unirse a evento</h1>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Nombre del evento</Table.HeaderCell>
+                        <Table.HeaderCell>Descripcion</Table.HeaderCell>
+                        <Table.HeaderCell>Integrantes</Table.HeaderCell>
+                        <Table.HeaderCell>Publico/Privado</Table.HeaderCell>
+                        <Table.HeaderCell>Valor</Table.HeaderCell>
+                        <Table.HeaderCell>Ubicacion</Table.HeaderCell>
+                        <Table.HeaderCell>Activo</Table.HeaderCell>
                     </Table.Row>
-                ))}
-            </Table.Body>
-        </Table>
-    );
-}
+                </Table.Header>
+    
+                <Table.Body>
+                    {events.map(event => (
+                        <Table.Row key={event.id}>
+                            <Table.Cell>{event.nombreevento}</Table.Cell>
+                            <Table.Cell>{event.descripcion}</Table.Cell>
+                            <Table.Cell>{event.integrantes}</Table.Cell>
+                            <Table.Cell>{event.publicooprivado}</Table.Cell>
+                            <Table.Cell>{event.valor}</Table.Cell>
+                            <Table.Cell>{event.ubicacion}</Table.Cell>
+                            <Table.Cell>{event.activo}</Table.Cell>
+                        </Table.Row>))}
+                </Table.Body>
+            </Table>
+        </div>
+    )}
+
+
+    
