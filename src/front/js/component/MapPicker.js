8de129743338eabb7b6be4c4776ac77c6063db36
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2023-02-01 15:23:18 +0000

    ubicacion actualizada mapa scl

--- src/front/js/component/MapPicker.js
+++ src/front/js/component/MapPicker.js
@@ -7,7 +7,7 @@ const MapPicker = () => {
   const [selectedLocation, setSelectedLocation] = useState(null);
 
   useEffect(() => {
-    mapRef.current = L.map('map').setView([51.505, -0.09], 13);
+    mapRef.current = L.map('map').setView([-33.4488897, -70.6692655], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
